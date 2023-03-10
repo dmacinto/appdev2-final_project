6061b599ce3db2d6bb4ac824b1352d89026903fa
dmacinto <dmacinto@gmail.com>
2023-03-10 03:12:29 +0000

    at least I can get the cancel to work

--- src/components/TaskFeed/TaskFeed.js
+++ src/components/TaskFeed/TaskFeed.js
@@ -1,8 +1,10 @@
 import React from "react";
-import { View, Text, FlatList, StyleSheet, Button, TextInput } from "react-native";
+import { View, Text, FlatList, StyleSheet, Button, TouchableOpacity, Modal } from "react-native";
 import { axiosInstance } from "../../utils";
 import SignOut from "../SignOut/SignOut";
 import CreateTask from "../CreateTask/CreateTask";
+import { TextInput } from "react-native-gesture-handler";
+
 
 
 function Task({ task }) {
@@ -17,6 +19,18 @@ function Task({ task }) {
 
 function TaskFeed({ user }) {
     const [tasks, setTasks] = React.useState([]);
+    const [isRender, setisRender] = React.useState(false);
+    const [isModalVisible, setisModalVisible] = React.useState(false);
+    const [inputText, setinputText] = React.useState();
+    const [editTask, seteditTask] = React.useState();
+    
+    
+    const onPressTask = (task) => {
+        setisModalVisible(true);
+        setinputText(task.body);
+        seteditTask(task.id)
+    }
+
 
     function ListHeader() {
         return (
@@ -34,6 +48,36 @@ function TaskFeed({ user }) {
         }
     }
 
+    const renderItem = ({item, index}) => {
+        return(
+            <TouchableOpacity
+                style={styles.item}
+                onPress={()=> onPressTask(item)}
+            >
+                <Task task={item}/>
+            </TouchableOpacity>
+        )
+    }
+
+    const handleEditTask = (editTask) => {
+        const changeData = tasks.map(async task => {
+            if (task.id == editTask) {
+                task.body = inputText;
+                //task.status = task.status;
+                await axiosInstance.patch(`/tasks/${task.id}/edit.json`, task.body);
+                return task;
+            }
+            return task;
+        })
+        setTasks(changeData);
+        setisRender(!isRender);
+    }
+
+    const onPressSaveEdit = () => {
+        handleEditTask(editTask);
+        setisModalVisible(false);
+    }
+
     React.useEffect(() => {
         getFeed()
     }, [])
@@ -42,12 +86,51 @@ function TaskFeed({ user }) {
         <View>
             <FlatList
                 data={tasks}
-                renderItem={({item}) => <Task task={item} />}
+                renderItem={renderItem }
                 keyExtractor={item => item.id}
                 contentContainerStyle={styles.contentContainer}
                 ListHeaderComponent={ListHeader}
-                
+                extraData={isRender}
             />
+            <Modal
+                animationType='fade'
+                visible={isModalVisible}
+                onRequestClose={() => setisModalVisible(false)} >
+
+            <View style={styles.modalView}>
+                <Text style={styles.text}>
+                    Change Task
+                </Text>
+                <TextInput
+                    style={styles.textInput}
+                    onChangeText={(text)=> setinputText(text)}
+                    defaultValue={inputText}
+                    editable={true}
+                    multiline={false}
+
+                />
+                
+                <TouchableOpacity
+                 onPress={() =>onPressSaveEdit()}
+                 style={styles.touchableSave}
+                >
+                    <Text style={styles.text}>Save</Text>
+
+                </TouchableOpacity>
+
+                <TouchableOpacity
+                 onPress={() =>setisModalVisible(false)}
+                 style={styles.touchableSave}
+                >
+                    <Text style={styles.text}>Cancel</Text>
+
+                </TouchableOpacity>
+
+            </View>
+
+
+            </Modal>
+
 
 
             <Button
@@ -79,8 +162,11 @@ const styles = StyleSheet.create({
         textAlign: 'center'
     },
     contentContainer: {
-        paddingTop: 80,
-        paddingHorizontal: 20
+        paddingHorizontal: 20,
+        borderBottomColor: 'grey',
+        borderBottomWidth: 1,
+        alignItems: 'center',
+        borderTopWidth:1
     },
     input: {
         height: 40,
@@ -88,7 +174,31 @@ const styles = StyleSheet.create({
         marginBottom: 12,
         borderWidth: 1,
         padding: 10,
-      }
+      },
+    item: {
+        borderBottomWidth: 1,
+        borderBottomColor:'grey',
+        alignItems: 'center',
+        fontSize: 24
+      },
+    textInput:{
+        width: '90%',
+        height: 70,
+        borderColor: 'grey',
+        borderWidth: 1,
+        fontSize: 25
+      },
+    modalView:{
+        flex:1,
+        alignItems: 'center',
+        justifyContent: 'center'
+    },
+    touchableSave: {
+        backgroundColor: 'orange',
+        paddingHorizontal: 10,
+        alignItems: 'center',
+        marginTop: 20
+    }
 })
 
 
